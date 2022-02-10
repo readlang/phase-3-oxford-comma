b3ae8ca4e289933d0608ab94f3b4375fc1a357eb
Read Langworthy <rlangworthy@gmail.com>
2022-02-10 13:52:08 -0800

    commit tree 1300ed03fceebd76d1943e4ea085370515eb3cdb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
+
+
+
+def oxford_comma (array)
+    string = ""
+    
+    i = 0
+    while i < (array.length - 1)
+        string += "#{array[i]}, "
+        i += 1
+    end
+
+    if array.length > 1
+        string += "and #{array[-1]}"    
+    else
+        return array[0]
+    end
+    
+    return string
 
 end
+
+
+puts oxford_comma ["x", "y", "z"]
